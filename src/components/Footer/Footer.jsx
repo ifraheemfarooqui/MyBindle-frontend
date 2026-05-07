4ed6f0d32538206b60a25bfdf3cd257b0c8f6f67
Ifraheem Farooqui <ifraheemfarooqui@gmail.com>
2026-05-07 22:32:12 +0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -63,11 +63,9 @@ export default function Footer() {
               onMouseEnter={e => gsap.to(e.currentTarget, { scale: 1.06, duration: 0.25 })}
               onMouseLeave={e => gsap.to(e.currentTarget, { scale: 1,    duration: 0.25 })}
             >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="store-icon">
-                <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.8-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
-              </svg>
+<img className='store-icon' src="src\assets\apple-logo.png" alt="" />
               <div>
-                <span className="store-label">Download on the</span>
+                <span className="store-label">Download on the</span><br />
                 <span className="store-name">App Store</span>
               </div>
             </a>
@@ -79,11 +77,9 @@ export default function Footer() {
               onMouseEnter={e => gsap.to(e.currentTarget, { scale: 1.06, duration: 0.25 })}
               onMouseLeave={e => gsap.to(e.currentTarget, { scale: 1,    duration: 0.25 })}
             >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="store-icon">
-                <path d="M3 20.5v-17c0-.83 1-.95 1.46-.5l14 8.5c.4.24.4.83 0 1.07L4.46 21c-.46.45-1.46.33-1.46-.5zM5 7.07V12h5l-5-4.93zM5 12v4.93L10 12H5zm7.19 0L7.93 16.27 17.38 12l-5.19 0zm0 0l5.19 0L7.93 7.73 12.19 12z"/>
-              </svg>
+             <img src="src\assets\google-play 1.png" alt="" />
               <div>
-                <span className="store-label">Get it on</span>
+                <span className="store-label">Get it on</span><br />
                 <span className="store-name">Google Play</span>
               </div>
             </a>
